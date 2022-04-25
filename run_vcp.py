ab1d5f6493000024c055341341de131b1153362c
dtilley <drew.tilley@gmail.com>
2022-04-25 12:40:59 -0400

    changed interpolation bounds.

--- run_vcp.py
+++ run_vcp.py
@@ -55,9 +55,11 @@ def run_vcp(ind_params, model_ndx, vc_data, dt=0.1, protocol_name='vcp', rtrn=Fa
     # Simulate VCP
     vcp_response = kc.generate_response(vcp, is_no_ion_selective=False)
     # Remove v_hold segments
-    t_bounds = [np.abs(vcp_response.t - 10000).argmin(),
-                np.abs(vcp_response.t - (10000 + t[-1])).argmin()+1]
-    t_simu = vcp_response.t[t_bounds[0]:t_bounds[1]] - vcp_response.t[t_bounds[0]]
+    t_bounds = [(np.abs(vcp_response.t - 9990).argmin()),
+                (len(vcp_response.t)-1)]
+    t_simu = vcp_response.t[t_bounds[0]:t_bounds[1]] - 10000.0
+    print('t_bounds: ', t_bounds)
+    print('t_simu[0]: ', t_simu[0], ' t_simu[-1]: ', t_simu[-1])
     # Check bounds
     if (t_simu[0] <= t[0] and t_simu[-1] >= t[-1]):
         # Get simulated currents
@@ -89,35 +91,7 @@ def run_vcp(ind_params, model_ndx, vc_data, dt=0.1, protocol_name='vcp', rtrn=Fa
             return
     else:
         print('Interpolation bounds error with model: '+str(model_ndx))
-        print('Retrying...')
-        t_bounds[0] = t_bounds[0]-1
-        t_bounds[1] = t_bounds[1]+1
-        t_simu = vcp_response.t[t_bounds[0]:t_bounds[1]] - vcp_response.t[t_bounds[0]]
-        if (t_simu[0] <= t[0] and t_simu[-1] >= t[-1]):
-            # Get simulated currents
-            currents = list()
-            I_total = vcp_response.current_response_info.get_current_summed()[t_bounds[0]:t_bounds[1]]
-            f = interp1d(t_simu, I_total)
-            currents.append(f(t))
-            # Current Dictionary
-            current_names = ['I_K1', 'I_To', 'I_Kr', 'I_Ks', 'I_CaL', 'I_NaK', 'I_Na',
-                             'I_NaCa', 'I_pCa', 'I_F', 'I_bNa', 'I_bCa', 'I_CaT']
-            for i in current_names:
-                current = vcp_response.current_response_info.get_current(i)
-                f = interp1d(t_simu, current[t_bounds[0]:t_bounds[1]])
-                currents.append(f(t))
-        current_names = ['I_total'] + current_names
-        current_names.append('t')
-        current_names.append('vcmd')
-        currents.append(t)
-        currents.append(vcmd)
-
-        # Write Data
-        filename = 'model_' + str(model_ndx) + '_' + protocol_name + '.txt'
-        data = pd.DataFrame(np.column_stack(currents), columns=current_names)
-        data.to_csv(filename, sep=' ', index=False)
-
         if (rtrn):
-            return((current_names, currents))
+            return (('_', 'interpolation_error'))
         else:
             return
